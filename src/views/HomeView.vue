b4234d3632b49dfca498fec054f4d655c8efc9de
hochstegerL03 <72703264+hochstegerL03@users.noreply.github.com>
2022-10-15 23:31:47 +0200

    V0.0.9
    EtS-Banner removed - Image Bug Fix

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,10 +2,7 @@
   <!--Header-->
   <div>
     <!--Default Banner. Can be modified -->
-    <et-s-banner
-      class="q-mb-md bannerlong"
-      img="url('/images/home_gears_banner.jpg')"
-    ></et-s-banner>
+    <q-img class="q-mb-md bannerlong" src="images/home_gears_banner.jpg"></q-img>
     <!--Banner End-->
     <!--Infos-->
     <et-s-header
@@ -30,88 +27,82 @@
       <div class="flex justify-center items-center q-mb-xl">
         <!--Placeholder Lection Banner-->
         <div id="news" class="bannernewscontainer q-py-md">
-          <et-s-banner class="">
-            <div class="bannernewsoverlay bannernews placeholder">
-              <div class="flex items-start justify-center">
-                <header class="fitbanner">
-                  <p class="q-pa-md header text-italic text-h5">
-                    Just an Act: Learn the Magic of Web-Development!
-                  </p>
-                </header>
-              </div>
-              <main></main>
-              <footer class="absolute-bottom barabsolute">
-                <div class="row">
-                  <div class="col-8">
-                    <div class="bar-wrapper">
-                      <div class="lightbar">
-                        <div class="flex items-center justify-center h-100">
-                          <div
-                            class="text-white text-center q-pb-xs subheader text-weight-bold text-italic text-h6"
-                          >
-                            HTML, JavaScript
-                          </div>
+          <div class="bannernewsoverlay bannernews placeholder">
+            <div class="flex items-start justify-center">
+              <header class="fitbanner">
+                <p class="q-pa-md header text-italic text-h5">
+                  Just an Act: Learn the Magic of Web-Development!
+                </p>
+              </header>
+            </div>
+            <main></main>
+            <footer class="absolute-bottom barabsolute">
+              <div class="row">
+                <div class="col-8">
+                  <div class="bar-wrapper">
+                    <div class="lightbar">
+                      <div class="flex items-center justify-center h-100">
+                        <div
+                          class="text-white text-center q-pb-xs subheader text-weight-bold text-italic text-h6"
+                        >
+                          HTML, JavaScript
                         </div>
                       </div>
-                      <div class="darkbar">
-                        <div class="flex items-center justify-end h-100">
-                          <div class="darkbarcontent">
-                            <span
-                              class="text-white text-left subheader text-weight-bold text-italic text-h5"
-                              >...</span
-                            >
-                          </div>
+                    </div>
+                    <div class="darkbar">
+                      <div class="flex items-center justify-end h-100">
+                        <div class="darkbarcontent">
+                          <span
+                            class="text-white text-left subheader text-weight-bold text-italic text-h5"
+                            >...</span
+                          >
                         </div>
                       </div>
                     </div>
                   </div>
-                  <div class="col-4">
-                    <span
-                      class="text-primary float-right fitbanner text-center subheader text-weight-bolder text-h5"
-                      >New!</span
-                    >
-                  </div>
                 </div>
-              </footer>
-            </div>
-          </et-s-banner>
+                <div class="col-4">
+                  <span
+                    class="text-primary float-right fitbanner text-center subheader text-weight-bolder text-h5"
+                    >New!</span
+                  >
+                </div>
+              </div>
+            </footer>
+          </div>
         </div>
         <!--Placeholder Lection Banner End-->
 
         <!--Placeholder Lection Info Banner-->
         <div id="newsdetails" class="bannernewscontainer-flex q-py-md">
-          <et-s-banner class="">
-            <div
-              class="bannernewsoverlay bannernewsdetails { border-radius: 0vw 0vw 5vw 5vw; } placeholder"
-            >
-              <header>
-                <div class="header-30">
-                  <div class="row items-start justify-center h-100">
-                    <div class="infobar col-3">
-                      <div
-                        class="text-white text-center q-pb-xs subheader text-weight-bold text-italic text-h6"
-                      >
-                        Info
-                      </div>
+          <div
+            class="bannernewsoverlay bannernewsdetails { border-radius: 0vw 0vw 5vw 5vw; } placeholder"
+          >
+            <header>
+              <div class="header-30">
+                <div class="row items-start justify-center h-100">
+                  <div class="infobar col-3">
+                    <div
+                      class="text-white text-center q-pb-xs subheader text-weight-bold text-italic text-h6"
+                    >
+                      Info
                     </div>
-
-                    <div class="col-6 self-end">
-                      <div class="header text-center self-end text-italic text-h5">
-                        Just an Act:
-                      </div>
-                    </div>
-                    <div class="col-3"></div>
                   </div>
+
+                  <div class="col-6 self-end">
+                    <div class="header text-center self-end text-italic text-h5">Just an Act:</div>
+                  </div>
+                  <div class="col-3"></div>
                 </div>
-              </header>
-              <main class="q-pb-lg">
-                <div class="text-h6 text-weight-light q-pa-md text-italic">
-                  Learn more about the Beauty behind programming. Discover a breathtaking world
-                  ruled by a fool scheme ...
-                </div>
-              </main>
-            </div>
-          </et-s-banner>
+              </div>
+            </header>
+            <main class="q-pb-lg">
+              <div class="text-h6 text-weight-light q-pa-md text-italic">
+                Learn more about the Beauty behind programming. Discover a breathtaking world ruled
+                by a fool scheme ...
+              </div>
+            </main>
+          </div>
         </div>
         <!--Placeholder Lection Info Banner End-->
       </div>
@@ -177,7 +168,7 @@
           <!--Chapter 1-->
           <!--Image-->
           <figure class="w-100 q-ma-none q-mt-lg row text">
-            <q-img src="/images/mathe_meme.png" />
+            <q-img src="images/mathe_meme.png" />
             <figcaption class="text-weight-light text-italic w-90 q-mt-xs">
               - HTL Wien West Deluxe Memes
             </figcaption>
@@ -431,7 +422,6 @@
 }
 </style>
 <script setup>
-import EtSBanner from '../components/EtSBanner.vue';
 import EtSChatNav from '../components/EtSChatNav.vue';
 import EtSHeader from '../components/EtSHeader.vue';
 import { scroll } from 'quasar';
