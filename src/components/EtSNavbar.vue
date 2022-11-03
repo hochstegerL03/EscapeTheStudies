989aca1d6e7624806f2ed7948103f6d44ea2fe37
hochstegerL03 <72703264+hochstegerL03@users.noreply.github.com>
2022-11-03 08:06:01 +0100

    V.0.1.4
    "logo font" added (navbar)

--- src/components/EtSNavbar.vue
+++ src/components/EtSNavbar.vue
@@ -7,7 +7,7 @@
         </q-avatar>
       </div>
       <div class="col-6 text-center">
-        <span class="subheader">Escape the Studies</span>
+        <span class="subheader"><b>E</b>scape <b>t</b>he <b>S</b>tudies</span>
       </div>
       <div class="col-3">
         <q-btn
