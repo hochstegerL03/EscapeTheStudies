bd92496e96be49e1a46f61f7446038ffae17b263
Raphael Losko <losko.r04@htlwienwest.at>
2023-02-23 11:10:18 +0100

    route repariert

--- etsserver/controllers/ets.js
+++ etsserver/controllers/ets.js
@@ -3,6 +3,6 @@ import { dbGetCourse, dbGetQuesition } from '../models/ets.js';
 
 const getCourse = async (req, res) => res.status(200).json(await dbGetCourse());
 
-const getQuestion = async (req, res) => res.status(200).json(await dbGetQuestion());
+const getQuestion = async (req, res) => res.status(200).json(await dbGetQuesition());
 
 export { getCourse, getQuestion };
