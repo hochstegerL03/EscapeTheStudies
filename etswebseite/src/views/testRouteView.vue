5f4cc5abb1c63fd61492c3dce760d28342421bb2
Raphael04-lo <losko.r04@htlwienwest.at>
2023-02-25 16:28:15 +0100

    new View for testing pinia store

---
+++ etswebseite/src/views/testRouteView.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { ref, onMounted } from 'vue';
+import { useCourseStore } from '../stores/course.js';
+
+const courseStore = useCourseStore();
+let course = ref([]);
+let answer = ref([]);
+let question = ref([]);
+
+onMounted(async () => {
+  await courseStore.getCourse();
+  await courseStore.getQAndA();
+  course.value = courseStore.course;
+  answer.value = courseStore.answer;
+  question.value = courseStore.question;
+});
+</script>
+
+<template>
+  <div>
+    <h4>{{ course }}</h4>
+    <h4>{{ answer }}</h4>
+    <h4>{{ question }}</h4>
+  </div>
+</template>
