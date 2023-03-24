9afd31d823781f599265d3b7feb6adb6aa3ab69b
Raphael Losko <losko.r04@htlwienwest.at>
2023-03-24 11:26:09 +0100

    .env datei parameter namen geändert

--- etsserver/config/supabaseClient.js
+++ etsserver/config/supabaseClient.js
@@ -3,8 +3,8 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
-const supabaseKey = process.env.REACT_APP_ANON_KEY;
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_ANON_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
