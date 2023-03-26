f67b676e11598d72a52c42bb11f9e2c5e5d73317
Raphael04-lo <losko.r04@htlwienwest.at>
2023-03-26 15:23:13 +0200

    question build in, trying to get answer

--- etswebseite/src/views/just_an_act/Chapter2View.vue
+++ etswebseite/src/views/just_an_act/Chapter2View.vue
@@ -38,6 +38,71 @@
       <div class="ets-w-80" v-html="lecture" v-if="lecture"></div>
     </div>
   </div>
+  <div id="questions" class="flex justify-center">
+    <!--Body/Text-->
+    <div class="ets-w-80">
+      <!--Header-->
+      <div>
+        <div class="text text-h4 text-weight-regular q-my-md">Questions</div>
+      </div>
+      <!--Header End-->
+      <!--Body/Text End-->
+    </div>
+    <!--Part 1: Story End-->
+  </div>
+  <div class="flex justify-center">
+    <div class="ets-w-80">
+      <q-form>
+        <div v-for="question in questions" :key="question.id">
+          <!--Multiple Choice Question-->
+          <EtSQuestionMutlipleChoice
+            v-if="question.questiontype.questiontype == 'MultipleChoice'"
+            @changeAnswer="changeAnswer"
+            :question="question"
+          ></EtSQuestionMutlipleChoice>
+          <!--Multiple Choice Question End-->
+          <!--Text Input Question-->
+          <EtSQuestionTextInput
+            v-else-if="question.type == 'TextInput'"
+            @changeAnswer="changeAnswer"
+            :question="question"
+          >
+          </EtSQuestionTextInput>
+          <!--Text Input Question End-->
+          <!--Build Answer Question-->
+          <EtSQuestionBuildAnswer
+            v-else-if="question.type === 'Text Ordering'"
+            @changeAnswer="changeAnswer"
+            :question="question"
+          ></EtSQuestionBuildAnswer>
+          <!--Build Answer Question End-->
+        </div>
+      </q-form>
+    </div>
+    <!--Questions End-->
+    <!--Checkbar-->
+    <div class="ets-w-100 row justify-center items-start q-my-xl">
+      <div class="col-6">
+        <div
+          class="text-center ets-fake-button text-secondary ets-header text-h5 text-weight-bold"
+          @click="trySub()"
+        >
+          Check
+        </div>
+        <div class="text-center ets-header text-grey text-caption text-italic text-weight-light">
+          <span v-if="falseQ.length > 0">{{ falseQ.length }} Error/s found.</span>
+          <span v-else>0 Errors found.</span>
+          <span class="ets-underline">click here!</span>
+        </div>
+      </div>
+      <div class="col-6 text-center ets-header text-h6 text-weight-bold">
+        <div class="text-center text-secondary ets-header text-h5 text-weight-bold">Go Next!</div>
+        <div class="text-center ets-header text-grey text-caption text-italic text-weight-light">
+          Check your Answers to get a Star
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 <style lang="scss" scoped>
 .ets-absolute-top {
@@ -55,11 +120,14 @@ import { scroll } from 'quasar';
 import { ref, onMounted } from 'vue';
 import { useTextData } from '../../stores/textdata.js';
 import axios from 'axios';
+import EtSQuestionMutlipleChoice from '../../components/EtSQuestionMutlipleChoice.vue';
+import EtSQuestionTextInput from '../../components/EtSQuestionTextInput.vue';
+import EtSQuestionBuildAnswer from '../../components/EtSQuestionBuildAnswer.vue';
 
 const textStore = useTextData();
 let lecture = ref();
 let story = ref();
-let question = ref([]);
+let questions = ref([]);
 let answer = ref([]);
 
 onMounted(async () => {
@@ -70,142 +138,13 @@ onMounted(async () => {
   const serQ = await axios.get(
     'http://localhost:3000/escapethestudies/question?title=Chapter 2: Was ist HTML - The Constructor',
   );
-  question.value = serQ.data;
-  console.log(question);
+  questions.value = serQ.data;
+  console.log(questions.value[0].questiontype.questiontype);
   const serA = await axios.get('http://localhost:3000/escapethestudies/answers');
-  console.log(serA.data);
-  for (let index = 0; index < question.value.length; index++) {
-    answer.value.push(serA.data.filter((el) => el.questionid == question.value[index].questionid));
+  for (let index = 0; index < questions.value.length; index++) {
+    answer.value.push(serA.data.filter((el) => el.questionid == questions.value[index].questionid));
   }
-  console.log(answer);
 });
-
-const questions = ref([
-  {
-    id: 1,
-    type: 'multipleChoice',
-    description:
-      'Lass uns damit mal mit ein paar einfachen und allgemeinen Fragen starten! Achtung, nicht alle Fragen sind in der derzeitigen Lektion erklärt!',
-    question:
-      'Welche drei “Programmiersprachen” werden für eine moderne/zeitgemäße Website benötigt?',
-    answers: ['JavaScript, HTML, CSS.', 'JavaScript, Vue, C#.', 'HTML ,CSS, Python.'],
-    showedAnswer: '',
-    correctAnswer: 'JavaScript, HTML, CSS.', //hier gehört ein Regex hin
-  },
-  {
-    id: 2,
-    type: 'multipleChoice',
-    description: '',
-    question: 'Welchen Fehler hatte die erste Frage?',
-    answers: [
-      'CSS & HTML sind eigentlich Beschreibungssprachen.',
-      'JavaScript ist eigentlich eine Beschreibungssprache.',
-      'Es gab keinen Fehler: Trick-Question!',
-    ],
-    showedAnswer: '',
-    correctAnswer: 'CSS & HTML sind eigentlich Beschreibungssprachen.', //hier gehört ein Regex hin
-  },
-  {
-    id: 3,
-    type: 'textInput',
-    description: '',
-    question:
-      'Deklariere drei Variablen: Die Konstante “change” mit dem Wert “Life always Changes”, die globale Variable “climateChange” mit dem booleschen Wert, “true”, und die lokale Variable “you” ohne Wertangabe:',
-    showedAnswer: '',
-    correctAnswer: 'const change = “Life always Changes”; var climateChange = true; let you;', //hier gehört ein Regex hin
-  },
-  {
-    id: 4,
-    type: 'multipleChoice',
-    description: '',
-    question: 'Wie werden Datentypen in Vanilla JavaScript impliziert?',
-    answers: [
-      'Dynamisch: Der Datentyp wird mit dem angegebenen Wert definiert.',
-      'Statisch: Der Datentyp muss beim Anlegen der Variable angegeben werden.',
-    ],
-    showedAnswer: '',
-    correctAnswer: 'Dynamisch: Der Datentyp wird mit dem angegebenen Wert definiert.', //hier gehört ein Regex hin
-  },
-  {
-    id: 5,
-    type: 'multipleChoice',
-    description: '',
-    question: 'Mit welchen dieser Tags kann kein Text in HTML angezeigt werden?',
-    answers: ['p', 'h1', 'h6', 'div', 'br'],
-    showedAnswer: '',
-    correctAnswer: 'br', //hier gehört ein Regex hin
-  },
-  {
-    id: 6,
-    type: 'multipleChoice',
-    description: '',
-    question: 'Wie reagiert HTML auf folgenden Code? <h187>Werde ich angezeigt?</h187>',
-    answers: [
-      'Es wird ein Fehler auf der Konsole ausgeworfen und der Text ignoriert.',
-      'Der Text wird trotz falschem Tag angezeigt, jedoch mit den Font Values vom parent-Object.',
-      'Die Webseite kann wegen dem Fehler nicht laden.',
-    ],
-    showedAnswer: '',
-    correctAnswer:
-      'Der Text wird trotz falschem Tag angezeigt, jedoch mit den Font Values vom parent-Object.', //hier gehört ein Regex hin
-  },
-  {
-    id: 7,
-    type: 'multipleChoice',
-    description: '',
-    question: 'Welcher Output wird bei folgender Addition gegeben: console.log(“16” + 16);',
-    answers: ['1616', '32', 'N.a.N'],
-    showedAnswer: '',
-    correctAnswer: '1616', //hier gehört ein Regex hin
-  },
-  {
-    id: 8,
-    type: 'buildAnswer',
-    description: '',
-    question:
-      'Konstruiere eine einfache Funktion namens “double” in Javascript mit folgenden Parameter, “number”, welcher den Eingabeparameter mit sich selbst multipliziert und zurückgibt:',
-    answers: [
-      'function',
-      '(number).',
-      'double',
-      '{',
-      '}',
-      'const',
-      'return',
-      'number * number;',
-      'square(number);',
-      '(double)',
-      'number',
-    ],
-    showedAnswer: [],
-    correctAnswer: 'function double (number) { return number * number; }', //hier gehört ein Regex hin
-  },
-  {
-    id: 9,
-    type: 'multipleChoice',
-    description: '',
-    question: 'Welcher der folgenden CSS Attribute ändert die Farbe der Schrift zu rot?',
-    answers: [
-      'background-color: red;',
-      'color: rot;',
-      'font-color: red;',
-      'color: red;',
-      'font-color: #ff0000;',
-      'display-color: red;',
-    ],
-    showedAnswer: '',
-    correctAnswer: 'color: red;', //hier gehört ein Regex hin
-  },
-  {
-    id: 10,
-    type: 'multipleChoice',
-    description: '',
-    question: 'Welche Version von HTML ist derzeit in Benutzung?',
-    answers: ['HTML3.5', 'HTML5', 'HTML9', 'HTML4'],
-    showedAnswer: '',
-    correctAnswer: 'HTML5', //hier gehört ein Regex hin
-  },
-]);
 
 function changeAnswer(answer, id) {
   questions.value[questions.value.findIndex((el) => el.id == id)].showedAnswer = answer;
